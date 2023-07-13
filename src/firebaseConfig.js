7ab045fb127ed04e0d4449df462b70cfcedeb22a
VaibhavRaj45 <69836389+VaibhavRaj-lab@users.noreply.github.com>
2023-07-13 16:47:39 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -11,14 +11,7 @@ import { getDatabase, ref } from "firebase/database"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCeU0gIZI9Fv1nF7zn0MeOST3FyoSClKmA",
-  authDomain: "food-deli-129fe.firebaseapp.com",
-  databaseURL: "https://food-deli-129fe-default-rtdb.firebaseio.com",
-  projectId: "food-deli-129fe",
-  storageBucket: "food-deli-129fe.appspot.com",
-  messagingSenderId: "220910747897",
-  appId: "1:220910747897:web:229b2fd5ebe416be896228",
-  measurementId: "G-4V2NNDZETH"
+  "Your Firebase Config Json
 };
 
 // Initialize Firebase
